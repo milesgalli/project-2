305886b7e514f0dfe990aee5f1fe5671e5b4766f
Darciechapman <darciechapman96@gmail.com>
2020-06-02 16:15:36 +0800

    adding inputvaribles

--- public/js/create-hackathon.js
+++ public/js/create-hackathon.js
@@ -1,5 +1,18 @@
-$("#submit").on('click', function(){
+let titleInput = ("#inputHackathonTitle");
+let descriptionInput = ("#inputHackathonDiscription");
+let maxStudent = ("#inputNoHackathonStudents");
+let startDate = ("#inputHackathonStartDate");
+let endDate = ("#inputHackathonEndDate");
 
+$("#hackathonSubmit").on('click', function(){
+
+    let hackathonData = {
+        title: titleInput.val() ? titleInput.val().trim() : null,
+        description: descriptionInput ? descriptionInput.val().trim() : null,
+        maxStudent: maxStudentInput.val() ? maxStudentInput.val().trim() : null,
+        startDate: startDateNameInput.val() ? startDateNameInput.val().trim() : null,
+        endDate: endDateInput.val() ? endDateInput.val().trim() : null,
+    }
 
     $.ajax({url: '/api/hackathon', method: "post"})
 })
