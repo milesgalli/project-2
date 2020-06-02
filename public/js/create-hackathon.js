d30cf8027127d853e0cc6d3a0ef0c1936438f2ce
Darciechapman <darciechapman96@gmail.com>
2020-06-02 18:23:04 +0800

    hackathon sent to database

--- public/js/create-hackathon.js
+++ public/js/create-hackathon.js
@@ -1,28 +1,42 @@
 let titleInput = $("#inputHackathonTitle");
 let descriptionInput = $("#inputHackathonDiscription");
-let maxStudent = $("#inputNoHackathonStudents");
-let startDate = $("#inputHackathonStartDate");
-let endDate = $("#inputHackathonEndDate");
+let maxStudentInput = $("#inputNoHackathonStudents");
+let startDateInput = $("#inputHackathonStartDate");
+let endDateInput = $("#inputHackathonEndDate");
 
-$("#hackathonSubmit").on('click', function(){
+$("#hackathonSubmit").click(function () {
+    if (this.id =="hackathonSubmit") {
+        alert("clicked");
+    }
+})
+
+$("#hackathonSubmit").click(function (){
 
     let hackathonData = {
         title: titleInput.val() ? titleInput.val().trim() : null,
         description: descriptionInput ? descriptionInput.val().trim() : null,
         maxStudent: maxStudentInput.val() ? maxStudentInput.val().trim() : null,
-        startDate: startDateNameInput.val() ? startDateNameInput.val().trim() : null,
+        startDate: startDateInput.val() ? startDateInput.val().trim() : null,
         endDate: endDateInput.val() ? endDateInput.val().trim() : null,
     }
-
-    createHackathon(hackathonData)
+    console.log(hackathonData);
+    console.log(JSON.stringify(hackathonData));
+    createHackathon(hackathonData);
 })
 
 function createHackathon(hackathonData) {
     
-    $.ajax({url: '/api/hackathon', method: "post"})
+    $.ajax({
+        url: '/api/hackathons', 
+        method: "post", 
+        data: hackathonData
+    }).then(function(hackathonData) {
+        
+    }).catch(handleHackathonErr);
 }
+
 
 function handleHackathonErr(err) {
     $("#alert .msg").text(err.responseJSON);
     $("#alert").fadeIn(500);
-  }
+}
