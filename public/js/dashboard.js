a96bfaa06bd7b92cd1e7048de98e7adc3b7e8426
Pete Brennan <pweda64@hotmail.com>
2020-06-03 14:54:46 +0800

    fixed my issue with company ids not populating

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -29,7 +29,9 @@ function createHackathon(hackathonData) {
     method: "post",
     data: hackathonData,
   })
-    .then(function(hackathonData) {})
+    .then(function() {
+      location.reload();
+    })
     .catch(handleHackathonErr);
 }
 
@@ -40,24 +42,46 @@ function handleHackathonErr(err) {
 
 $(".hackathonDelete").on("click", function(event) {
   var id = $(this).data("id");
-  // Send the DELETE request.
-  $.ajax("/api/hackathons/" + id, {
-    type: "DELETE",
-  }).then(function() {
-    console.log("deleted hackathon ", id);
-    // Reload the page to get the updated list
-    location.reload();
-  });
-});
-
-$(".hackathonJoin").on("click", function(event) {
-    var id = $(this).data("id");
-    // Send the POST request.
-    $.ajax("/api/users/join-hackathon/" + id, {
-      type: "POST",
+  if (confirm("Are you sure you want to delete this Hackathon?")) {
+    // Send the DELETE request.
+    $.ajax("/api/hackathons/" + id, {
+      type: "DELETE",
     }).then(function() {
-      console.log("joined hackathon ", id);
+      console.log("deleted hackathon ", id);
       // Reload the page to get the updated list
       location.reload();
     });
-  });
+  }
+});
+
+$(".hackathonJoin").on("click", function(event) {
+  var id = $(this).data("id");
+  // Send the POST request.
+  $.ajax("/api/users/join-hackathon/" + id, {
+    type: "POST",
+  })
+    .then(function() {
+      console.log("joined hackathon ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    })
+    .catch(function(err) {
+      alert("You have already joined that hackathon");
+    });
+});
+
+$(".hackathonUnjoin").on("click", function(event) {
+  var id = $(this).data("id");
+  // Send the DELETE request.
+  $.ajax("/api/hackathons/unjoin/" + id, {
+    type: "DELETE",
+  })
+    .then(function() {
+      console.log("unjoined hackathon ", id);
+      // Reload the page to get the updated list
+      location.reload();
+    })
+    .catch(function(err) {
+      alert(err);
+    });
+});
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,8 +7,6 @@ module.exports = function(app) {
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
 
-  // could have api/student signup
-
   // LEAVE
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     // Sending back a password, even a hashed password, isn't a good idea
@@ -18,17 +16,11 @@ module.exports = function(app) {
     });
   });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-
   // LEAVE
-
+  // Route for signing up user if role = 0 then is a student user, if role = 1 then a company user
   app.post("/api/user/signup", function(req, res) {
     console.log(req.body);
-    // console.log(req.body);
     if (req.body.role === "0") {
-      // console.log("creating student");
       db.User.create({
         email: req.body.email,
         password: req.body.password,
@@ -46,20 +38,29 @@ module.exports = function(app) {
           res.status(422).json(err);
         });
     } else {
-      db.User.create({
-        email: req.body.email,
-        password: req.body.password,
-        fullName: req.body.fullName,
-        location: req.body.location,
-        role: req.body.role,
-        CompanyId: 1,
-      });
       db.Company.create({
         companyName: req.body.companyName,
         numberEmployees: req.body.numberEmployees,
         industry: req.body.industry,
         location: req.body.location,
       })
+        .then(
+          // gets the id of the latest company created and assigns it to the user (using a +1 as it actually gets the previous company - not the one just created???)
+          db.Company.findAll({
+            limit: 1,
+            order: [["createdAt", "DESC"]],
+          }).then(function(result) {
+            // console.log(result[0].dataValues.id);
+            db.User.create({
+              email: req.body.email,
+              password: req.body.password,
+              fullName: req.body.fullName,
+              location: req.body.location,
+              role: req.body.role,
+              CompanyId: result[0].dataValues.id + 1,
+            });
+          })
+        )
         .then(function() {
           res.redirect(307, "/api/login");
         })
@@ -69,70 +70,63 @@ module.exports = function(app) {
     }
   });
 
-  // redirect company to signup form
-
-  //post request
-
   // Route for logging user out
   app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
   });
 
-  // Route for getting some data about our user to be used client side
-  app.get("/api/user_data", function(req, res) {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.json({});
-    } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id,
-      });
-    }
-  });
-
-  // Get Route for finding all Hackathons
-
-  app.get("/api/hackathons/", function(req, res) {
-    db.Hackathon.findAll({}).then(function(dbHackathon) {
-      res.json(dbHackathon);
-    });
-  });
+  // // Get Route for finding all Hackathons
+  // app.get("/api/hackathons/", function(req, res) {
+  //   db.Hackathon.findAll({}).then(function(dbHackathon) {
+  //     res.json(dbHackathon);
+  //   });
+  // });
 
   // Get Route for retreieving a hackathon post from a specific company
 
-  //check when populated
-  app.get("/api/company/:companyName", function(req, res) {
-    db.Hackathon.findAll({
-      where: {
-        companyName: req.params.companyName,
-      },
-    }).then(function(dbCompany) {
-      res.json(dbCompany);
-    });
-  });
-
+  // //check when populated
+  // app.get("/api/company/:companyName", function(req, res) {
+  //   db.Hackathon.findAll({
+  //     where: {
+  //       companyName: req.params.companyName,
+  //     },
+  //   }).then(function(dbCompany) {
+  //     res.json(dbCompany);
+  //   });
+  // });
+  // route for a user joining a hackathon
   app.post("/api/users/join-hackathon/:id", function(req, res) {
     console.log(req.user);
     db.HackathonUser.create({
       UserId: req.user.id,
       HackathonId: req.params.id,
-    }).then(function(hackathon) {
-      res.json(hackathon);
-    });
+    })
+      .then(function(hackathon) {
+        res.json(hackathon);
+      })
+      .catch(function(err) {
+        res.status(422).json(err);
+      });
   });
 
-  app.get("/playground", function(req, res) {
-    db.User.findOne({ where: { id: 1 } }).then((user) => {
-      db.Hackathon.findOne({ where: { id: 1 } }).then((hackathon) => {
-        user.joinHackathon(hackathon);
-        res.json({ data: "ok" });
-      });
-    });
-  });
+  // app.post("/api/hackathon/hackathonUser", function(req, res) {
+  //   db.HackathonUser.create({
+  //     UserId: req.user,
+  //     HackathonId: req.body.HackathonId,
+  //   }).then(function() {
+  //     res.redirect(307, "/api/hackathons");
+  //   });
+  // });
+
+  // app.get("/playground", function(req, res) {
+  //   db.User.findOne({ where: { id: 1 } }).then((user) => {
+  //     db.Hackathon.findOne({ where: { id: 1 } }).then((hackathon) => {
+  //       user.joinHackathon(hackathon);
+  //       res.json({ data: "ok" });
+  //     });
+  //   });
+  // });
 
   // Post Route for Creating a new Post
 
@@ -140,7 +134,6 @@ module.exports = function(app) {
     // if(!req.user.CompanyId){
     //   res.json({errpr: "You are not a company user!!!"}).status(401)
     // }
-
     //checked
     db.Hackathon.create({
       title: req.body.title,
@@ -148,8 +141,7 @@ module.exports = function(app) {
       maxStudent: req.body.maxStudent,
       startDate: req.body.startDate,
       endDate: req.body.endDate,
-      // CompanyId: req.user.CompanyId
-      CompanyId: null, // FIXME: fix this req.user.CompanyId
+      CompanyId: req.user.CompanyId, 
     }).then(function(dbHackathon) {
       res.json(dbHackathon);
     });
@@ -166,66 +158,63 @@ module.exports = function(app) {
     });
   });
 
-  // route for updating hackathon posts
-
-  app.put("/api/hackathons", function(req, res) {
-    db.Hackathon.update(req.body, {
+  // Route for unjoining hackathon
+  app.delete("/api/hackathons/unjoin/:id", function(req, res) {
+    // console.log(UserId);
+    db.HackathonUser.destroy({
       where: {
-        title: req.body.title,
-        description: req.body.description,
-        maxStudent: req.body.maxStudent,
-        startDate: req.body.startDate,
-        endDate: req.body.endDate,
+        HackathonId: req.params.id,
+        UserId: req.user.id,
       },
     }).then(function(dbHackathon) {
       res.json(dbHackathon);
     });
   });
 
-  // LEAVE
-
-  app.post("/api/hackathon/hackathonUser", function(req, res) {
-    db.HackathonUser.create({
-      UserId: req.user,
-      HackathonId: req.body.HackathonId,
-    })
-      .then(function() {
-        res.redirect(307, "/api/hackathons");
-      })
-      .catch(function(err) {
-        res.status(422).json(err);
-      });
-  });
+  // // route for updating hackathon posts
+  // app.put("/api/hackathons", function(req, res) {
+  //   db.Hackathon.update(req.body, {
+  //     where: {
+  //       title: req.body.title,
+  //       description: req.body.description,
+  //       maxStudent: req.body.maxStudent,
+  //       startDate: req.body.startDate,
+  //       endDate: req.body.endDate,
+  //     },
+  //   }).then(function(dbHackathon) {
+  //     res.json(dbHackathon);
+  //   });
+  // });
 
   // Need to create one to get the userId from the hackathon
 
-  app.get("/api/hackathon/hackathonUser/:UserId", function(req, res) {
-    db.HackathonUser.findOne({
-      where: {
-        UserId: req.user,
-      },
-    })
-      .then(function() {
-        res.redirect(307, "/api/hackathons");
-      })
-      .catch(function(err) {
-        res.status(422).json(err);
-      });
-  });
+  // app.get("/api/hackathon/hackathonUser/:UserId", function(req, res) {
+  //   db.HackathonUser.findOne({
+  //     where: {
+  //       UserId: req.user,
+  //     },
+  //   })
+  //     .then(function() {
+  //       res.redirect(307, "/api/hackathons");
+  //     })
+  //     .catch(function(err) {
+  //       res.status(422).json(err);
+  //     });
+  // });
 
-  app.get("/api/hackathon/hackathonUser/:HackathonId", function(req, res) {
-    db.HackathonUser.findOne({
-      where: {
-        HackathonId: req.user.HackathonId,
-      },
-    })
-      .then(function() {
-        res.redirect(307, "/api/hackathons");
-      })
-      .catch(function(err) {
-        res.status(422).json(err);
-      });
-  });
+  // app.get("/api/hackathon/hackathonUser/:HackathonId", function(req, res) {
+  //   db.HackathonUser.findOne({
+  //     where: {
+  //       HackathonId: req.user.HackathonId,
+  //     },
+  //   })
+  //     .then(function() {
+  //       res.redirect(307, "/api/hackathons");
+  //     })
+  //     .catch(function(err) {
+  //       res.status(422).json(err);
+  //     });
+  // });
 
   // need to create one get the hackathon Id
 
